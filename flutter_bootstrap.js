f6dede61aca33f6e821ddb55d53bda005a01194a
chilusoft <chilusoft@users.noreply.github.com>
2026-06-03 16:06:34 +0000

    deploy: 8f8183dec15bba055de72373001aa04a5ea0b92a

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"85e694e481e53599e5330f2dcd97c08cd935c5
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3847020133" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "408337179" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
